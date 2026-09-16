0e04d5842ad54980b0a33e9269a45f1820f9b856
QuantumBen <omosabenedict@gmail.com>
2026-09-16 18:03:29 +0100

    Protected Routes

---
+++ src/routes/ProtectedRoute.tsx
@@ -0,0 +1,17 @@
+import { Navigate, Outlet } from "react-router-dom";
+
+import { useAppSelector } from "../app/hooks";
+
+export default function ProtectedRoute() {
+  const { user, initialized } = useAppSelector((state) => state.auth);
+
+  if (!initialized) {
+    return <div className="p-10 text-center">Checking authentication...</div>;
+  }
+
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return <Outlet />;
+}
