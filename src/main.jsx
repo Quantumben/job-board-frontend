fb493e35baa76b581effa13069c9d56a4d5646b1
QuantumBen <omosabenedict@gmail.com>
2026-09-16 18:02:05 +0100

    Understand the provider structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { BrowserRouter } from "react-router-dom";
+
+import { Provider } from "react-redux";
+
+import { QueryClientProvider,} from "@tanstack/react-query";
+
+import App from "./App";
+
+import { store } from "./app/store";
+import { queryClient } from "./app/queryClient";
+
+import "./index.css";
+
+createRoot(document.getElementById("root")! ).render(
+    <StrictMode>
+        <Provider store={store}>
+            <QueryClientProvider client={queryClient}>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </QueryClientProvider>
+        </Provider>
+    </StrictMode>
+);
