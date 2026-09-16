41c57efd9450bae3e48f65162643358c64cf594a
QuantumBen <omosabenedict@gmail.com>
2026-09-16 19:05:17 +0100

    Delete job

---
+++ src/features/jobs/hooks/useDeleteJob.ts
@@ -0,0 +1,21 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import { deleteJob } from '../../../api/jobs.api';
+
+export function useDeleteJob() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: deleteJob,
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['jobs'],
+      });
+
+      queryClient.invalidateQueries({
+        queryKey: ['my-jobs'],
+      });
+    },
+  });
+}
